b55d72703509e50c24bf7137ee8ffdb5f04e211d
fritzthecrisp <zephteoys@gmail.com>
2021-12-16 01:36:30 +0800

    added home page and link page and navbar and bootstrap

--- anna.js
+++ anna.js
@@ -1,1 +1,27 @@
-var toilet =[];
+flag=true;
+function toilety() {
+    var toilet = [];
+    
+    
+    if (flag==true) {
+        document.getElementById("demo").innerHTML = "hello world!";
+        flag=false;
+    }else{
+        document.getElementById("demo").innerHTML = "bye world!";
+        flag=true;
+    }
+    for (let index = 0; index < 9; index++) {
+        toilet[index] = index + index + 1;
+        console.log(toilet[index]);
+    }
+
+
+
+
+
+
+    console.log("hello world.")
+    console.log(toilet);
+
+}
+
